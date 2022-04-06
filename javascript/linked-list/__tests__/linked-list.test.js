c9fa5e107d579c24e785ebf4b71a485b1ec897fb
Beau Hibbert <beauhibbert789@gmail.com>
2022-04-05 17:18:15 -0700

    added tests

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,16 +1,49 @@
-// 'use strict';
+'use strict';
 
-// const { it } = require('eslint/lib/rule-tester/rule-tester');
-// // Require our linked list implementation
-// // const {LinkedList, Node} = require('../index');
+const LinkedList = require('../linked-list');
 
-// describe('Linked List', () => {
-//   it('works', () => {
-//     expect(true).toBeTruthy();
-//   });
-//   it('Should create a new linked list', () => {
-//     // let ll = new LinkedList();
-//     expect(ll.head).toBeNull();
-//   });
-// });
+describe('Testing Linked List and all methods', () => {
+  it('should create a new linked list', () => {
+    let list = new LinkedList;
+    expect(list.head).toEqual(null);
+  });
+  it('should properly insert a node', () => {
+    let list = new LinkedList;
+    list.insert('a');
+    expect(JSON.stringify(list)).toEqual('{"head":{"value":"a","next":null}}');
+  });
+  it('should point the head to first node', () => {
+    let list = new LinkedList;
+    list.insert('a');
+    expect(list.head.value).toEqual('a');
+  });
+});
+it('should properly insert multiple nodes', () => {
+  let list = new LinkedList;
+  list.insert('c');
+  list.insert('b');
+  list.insert('a');
+  expect(JSON.stringify(list)).toEqual('{"head":{"value":"a","next":{"value":"b","next":{"value":"c","next":null}}}}');
+});
+it('should return true when finding a value that exists in the list', () => {
+  let list = new LinkedList;
+  list.insert('c');
+  list.insert('b');
+  list.insert('a');
+  expect(list.includes('b')).toBeTruthy();
+});
+it('should return false when a value does not exist in the list', () => {
+  let list = new LinkedList;
+  list.insert('c');
+  list.insert('b');
+  list.insert('a');
+  expect(list.includes('d')).toBeFalsy();
+});
 
+it('should return a collection of all the values that exist in the linked list', () => {
+  let list = new LinkedList;
+  list.insert('c');
+  list.insert('b');
+  list.insert('a');
+  expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> NULL');
+});
