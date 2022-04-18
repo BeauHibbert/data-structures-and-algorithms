3796e565d9bc73198b4d32d05267c6f087ce5b75
Beau Hibbert <beauhibbert789@gmail.com>
2022-04-17 23:39:30 -0700

    README and tests

--- javascript/linked-list/README.md
+++ javascript/linked-list/README.md
@@ -1,4 +1,5 @@
 # Linked List
+
 - Can successfully instantiate an empty linked list
 - Can properly insert into the linked list
 - The head property will properly point to the first node in the linked list
@@ -7,8 +8,70 @@
 - Will return false when searching for a value in the linked list that does not exist
 - Can properly return a collection of all the values that exist in the linked list
 
+## Approach & Efficiency
+
+The efficiency for inserting an item into the list the efficency is O(1) because the 1 item is added at the beginning. For the includes and the toString, the efficency is O(n) because the worst case would be iterating through the whole linked list each time and it grows proportionally to n.
 
 <img width="1728" alt="Screen Shot 2022-04-05 at 4 24 18 PM" src="https://user-images.githubusercontent.com/91757275/161868082-c7a119b9-8402-484d-a2e4-1a42552763c4.png">
 
-## Approach & Efficiency
-The efficiency for inserting an item into the list the efficency is O(1) because the 1 item is added at the beginning. For the includes and the toString, the efficency is O(n) because the worst case would be iterating through the whole linked list each time and it grows proportionally to n.
+
+
+# Linked List Insertions
+
+Extend a Linked List to allow various insertion methods.
+
+Feature Tasks:
+Write the following methods for the Linked List class:
+
+- append
+  - arguments: new value
+  - adds a new node with the given value to the end of the list
+- insert before
+  - arguments: value, new value
+  - adds a new node with the given new value immediately before the first node that has the value specified
+- insert after
+  - arguments: value, new value
+  - adds a new node with the given new value immediately after the first node that has the value specified
+
+## Efficiency/Big O
+
+The big O space and time efficiency is O(n) because the worst case scenario is that it iterates through the whole linked list and n has to scale with it.
+
+<img width="1728" alt="linked list insertions" src="https://user-images.githubusercontent.com/91757275/163729023-72ad9598-a272-41ea-a2e8-5b6dc5358527.png">
+
+
+
+# Linked list k-th
+
+k-th value from the end of a linked list.
+
+Write the following method for the Linked List class:
+
+- kth from end
+- argument: a number, k, as a parameter.
+- Return the node’s value that is k places from the tail of the linked list.
+- You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
+
+## Efficiency/Big O
+
+The space and time efficiency would be O(n) because at worse it would have to iterate through the linked list once and grab the index value of the solution. It would have to scale with n.
+
+
+# Linked List Zip
+
+Zip two linked lists.
+
+Feature Tasks
+
+- Write a function called zip lists
+- Arguments: 2 linked lists
+- Return: New Linked List, zipped as noted below
+- Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the the zipped list.
+- Try and keep additional space down to O(1)
+- You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
+
+## Efficiency/Big O
+
+The space/time efficiency would be O(n^2) because the worst case is that it has to iterate through both lists and n would scale directly.
+
+<img width="1728" alt="linked list zip" src="https://user-images.githubusercontent.com/91757275/163728890-c4d9949a-1085-4170-b6c6-5e9cf6e529f3.png">
--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -2,6 +2,7 @@
 
 const LinkedList = require('../linked-list');
 
+
 describe('Testing Linked List and all methods', () => {
   it('should create a new linked list', () => {
     let list = new LinkedList;
@@ -46,4 +47,170 @@ it('should return a collection of all the values that exist in the linked list',
   list.insert('b');
   list.insert('a');
   expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> NULL');
+});
+
+// tests for code challenge 6
+
+it('Can successfully add a node to the end of the linked list', () => {
+  let list = new LinkedList;
+  list.insert('c');
+  list.insert('b');
+  list.insert('a');
+  list.append('d');
+  expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+});
+
+it('Can successfully add multiple nodes to the end of a linked list', () => {
+  let list = new LinkedList;
+  list.insert('c');
+  list.insert('b');
+  list.insert('a');
+  list.append('d');
+  list.append('e');
+  list.append('f');
+
+  expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> { e } -> { f } -> NULL');
+});
+
+it('Can successfully insert a node before a node located in the middle of a linked list', () => {
+  let list = new LinkedList;
+  list.append('a');
+  list.append('b');
+  list.insertBefore('b', 'z');
+
+
+  expect(list.toString()).toEqual('{ a } -> { z } -> { b } -> NULL');
+});
+
+it('Can successfully insert a node before the first node of a linked list', () => {
+  let list = new LinkedList;
+  list.append('a');
+  list.append('b');
+  list.insertBefore('a', 'z');
+
+
+  expect(list.toString()).toEqual('{ z } -> { a } -> { b } -> NULL');
+});
+
+it('Can successfully insert after a node in the middle of the linked list', () => {
+  let list = new LinkedList;
+  list.append('a');
+  list.append('b');
+  list.insertAfter('a', 'z');
+
+
+  expect(list.toString()).toEqual('{ a } -> { z } -> { b } -> NULL');
+});
+
+it('Can successfully insert a node after the last node of the linked list', () => {
+  let list = new LinkedList;
+  list.append('a');
+  list.append('b');
+  list.insertAfter('b', 'z');
+
+
+  expect(list.toString()).toEqual('{ a } -> { b } -> { z } -> NULL');
+});
+
+// tests for code challenge 7
+
+it('Where k is greater than the length of the linked list', () => {
+  let list = new LinkedList;
+  list.append('a');
+  list.append('b');
+
+  expect(list.kFromEnd(3)).toEqual('Exception');
+});
+
+it('Where k and the length of the list are the same', () => {
+  let list = new LinkedList;
+  list.append('a');
+  list.append('b');
+
+  expect(list.kFromEnd(2)).toEqual('Exception');
+});
+
+it('Where k is not a positive integer', () => {
+  let list = new LinkedList;
+  list.append('a');
+  list.append('b');
+
+  expect(list.kFromEnd(-1)).toEqual('Exception');
+});
+
+it('Where k is not a positive integer', () => {
+  let list = new LinkedList;
+  list.append('a');
+
+  expect(list.kFromEnd(0)).toEqual('a');
+});
+
+it('Where k is not a positive integer', () => {
+  let list = new LinkedList;
+  list.append('a');
+  list.append('b');
+  list.append('c');
+  list.append('d');
+  list.append('e');
+
+  expect(list.kFromEnd(2)).toEqual('c');
+});
+
+// code challenge 8 tests
+
+function llSetUp(arr, ll) {
+  arr.forEach(value => ll.append(value));
+}
+
+describe('Testing Linked List Zip function', () => {
+  it('should create a new ll equal to null if both ll are empty', () => {
+    const ll1 = new LinkedList;
+    const ll2 = new LinkedList;
+    let list = LinkedList.zipLists(ll1, ll2);
+    expect(list.head).toEqual(null);
+  });
+
+  it('should create a new ll equal to ll1 if ll2 is empty', () => {
+    const ll1 = new LinkedList;
+    const ll2 = new LinkedList;
+    llSetUp(['a', 'b', 'c'], ll1);
+    let list = LinkedList.zipLists(ll1, ll2);
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> NULL');
+  });
+
+  it('should create a new ll equal to ll2 if ll1 is empty', () => {
+    const ll1 = new LinkedList;
+    const ll2 = new LinkedList;
+    llSetUp(['x', 'y', 'z'], ll2);
+    let list = LinkedList.zipLists(ll1, ll2);
+    expect(list.toString()).toEqual('{ x } -> { y } -> { z } -> NULL');
+  });
+
+  it('should create a new ll with two ll of equal lengths', () => {
+    const ll1 = new LinkedList;
+    const ll2 = new LinkedList;
+    llSetUp(['a', 'b', 'c'], ll1);
+    llSetUp(['x', 'y', 'z'], ll2);
+    let list = LinkedList.zipLists(ll1, ll2);
+    expect(list.toString()).toEqual('{ a } -> { x } -> { b } -> { y } -> { c } -> { z } -> NULL');
+
+  });
+
+  it('should create a new ll with ll1 shorter than ll2', () => {
+    const ll1 = new LinkedList;
+    const ll2 = new LinkedList;
+    llSetUp(['a', 'b'], ll1);
+    llSetUp(['x', 'y', 'z'], ll2);
+    let list = LinkedList.zipLists(ll1, ll2);
+    expect(list.toString()).toEqual('{ a } -> { x } -> { b } -> { y } -> { z } -> NULL');
+  });
+
+  it('should create a new ll with ll2 shorter than ll1', () => {
+    const ll1 = new LinkedList;
+    const ll2 = new LinkedList;
+    llSetUp(['a', 'b', 'c'], ll1);
+    llSetUp(['x', 'y'], ll2);
+    let list = LinkedList.zipLists(ll1, ll2);
+    expect(list.toString()).toEqual('{ a } -> { x } -> { b } -> { y } -> { c } -> NULL');
+  });
 });
